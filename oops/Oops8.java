ee093b080f48d9263fe1b05f901b27b3645a5b96
karth <karth@KARTHIK>
2026-08-26 22:10:11 +0530

    Object Class Methods

--- oops/Oops8.java
+++ oops/Oops8.java
@@ -5,86 +5,60 @@ public class Oops8 {
     int id;
     String name;
 
-    Oops8(int id, String name) {
-        this.id = id;
-        this.name = name;
+    // No-argument constructor
+    Oops8() {
+        id = 101;
+        name = "Karthik";
     }
 
-    // 1. equals()
     @Override
     public boolean equals(Object obj) {
 
-        Oops7 s = (Oops7) obj;
+        if (this == obj)
+            return true;
+
+        if (!(obj instanceof Oops8))
+            return false;
+
+        Oops8 s = (Oops8) obj;
 
         return this.id == s.id && this.name.equals(s.name);
     }
 
-    // 2. finalize()
     @Override
     protected void finalize() throws Throwable {
-
         System.out.println("finalize() method called");
     }
 
-    // 3. getClass()
-    // No need to override getClass()
-    // It is directly inherited from Object
-
-    // 4. hashCode()
     @Override
     public int hashCode() {
-
         return id;
     }
 
-    // 5. toString()
     @Override
     public String toString() {
-
         return "ID: " + id + ", Name: " + name;
     }
 
     public static void main(String[] args) {
 
-        Oops7 s1 = new Oops7(101, "Karthik");
-        Oops7 s2 = new Oops7(101, "Ram");
-        Oops7 s3 = new Oops7(102, "Nandhan");
+        Oops8 s = new Oops8();
 
-        // 1. equals()
         System.out.println("----- equals() -----");
+        System.out.println(s.equals(s));
 
-        System.out.println(s1.equals(s2));
-        System.out.println(s1.equals(s3));
-
-
-        // 2. getClass()
         System.out.println("\n----- getClass() -----");
+        System.out.println(s.getClass());
+        System.out.println(s.getClass().getName());
 
-        System.out.println(s1.getClass());
-        System.out.println(s1.getClass().getName());
-
-
-        // 3. hashCode()
         System.out.println("\n----- hashCode() -----");
+        System.out.println(s.hashCode());
 
-        System.out.println(s1.hashCode());
-        System.out.println(s2.hashCode());
-        System.out.println(s3.hashCode());
-
-
-        // 4. toString()
         System.out.println("\n----- toString() -----");
+        System.out.println(s);
 
-        System.out.println(s1.toString());
-        System.out.println(s2);
-        System.out.println(s3.toString());
-
-
-        // 5. finalize()
         System.out.println("\n----- finalize() -----");
-
-        s1 = null;
-
+        s = null;
         System.gc();
     }
 }
